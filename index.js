ca1a866cb0f6d182d6dcfc33507554a21b311a42
Lukas Lamm <lukas.lamm89@gmail.com>
2016-04-28 20:19:33 +0200

    Add function to convert musicjson to array of notes

--- index.js
+++ index.js
@@ -2,6 +2,33 @@
   'use strict';
 
   var MusicJsonToolbox = {
+    /**
+     * Returns an array of all notes (transposed to C major)
+     * [ {object}, {object}, ... ]
+     * @param {object} obj - The musicjson object
+     */
+    notes: function(obj) {
+      var tempNotes = [];
+      var repeatStart = -1;
+
+      for (var i = 0; i < obj.measures.length; i++) {
+        if (obj.measures[i].attributes.repeat.left) {
+          repeatStart = i;
+        }
+        tempNotes = tempNotes.concat(obj.measures[i].notes);
+        if (obj.measures[i].attributes.repeat.right) {
+          if (repeatStart !== -1) {
+            while (repeatStart <= i) {
+              tempNotes = tempNotes.concat(obj.measures[repeatStart].notes);
+              repeatStart++;
+            }
+          }
+          repeatStart = -1;
+        }
+      }
+
+      return tempNotes;
+    }
   };
 
 
---
+++ test/notes.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+var MusicJsonToolbox = require('../index');
+
+var musicjson = require('./example.json');
+var notes = require('./notes.json');
+
+describe('MusicJsonToolbox.notes', function() {
+
+  it('creates array of notes from musicjson', function() {
+    var output = MusicJsonToolbox.notes(musicjson);
+    expect(JSON.stringify(output)).to.equal(JSON.stringify(notes));
+  });
+
+});
