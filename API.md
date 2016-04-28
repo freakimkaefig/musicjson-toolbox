8bde98236549eabd7991cbf80bd0d1b978602933
Lukas Lamm <lukas.lamm89@gmail.com>
2016-04-29 00:05:27 +0200

    Implement first version of comparing with ngrams

--- API.md
+++ API.md
@@ -5,15 +5,21 @@ The MusicJsonToolbox class implements static functions to operate with musicjson
 
 
 * [MusicJsonToolbox](#module_MusicJsonToolbox)
-    * [.notes(obj)](#module_MusicJsonToolbox.notes)
-    * [.ngrams(array, length)](#module_MusicJsonToolbox.ngrams)
+    * [.notes(obj)](#module_MusicJsonToolbox.notes) ⇒ <code>Array</code>
+    * [.ngrams(array, length)](#module_MusicJsonToolbox.ngrams) ⇒ <code>Array</code>
+    * [.base12Pitch(step, octave, alter, absolute)](#module_MusicJsonToolbox.base12Pitch) ⇒ <code>number</code>
+    * [.comparePitch(pitch1, pitch2, absolute)](#module_MusicJsonToolbox.comparePitch) ⇒ <code>number</code>
+    * [.compareDuration(duration1, duration2)](#module_MusicJsonToolbox.compareDuration) ⇒ <code>number</code>
+    * [.uniques(array)](#module_MusicJsonToolbox.uniques) ⇒ <code>Array</code>
+    * [.distanceNgrams(object, search)](#module_MusicJsonToolbox.distanceNgrams) ⇒ <code>object</code>
 
 <a name="module_MusicJsonToolbox.notes"></a>
 
-### MusicJsonToolbox.notes(obj)
+### MusicJsonToolbox.notes(obj) ⇒ <code>Array</code>
 Returns an array of all notes (transposed to C major).Example:[ {object}, {object}, ... ]
 
 **Kind**: static method of <code>[MusicJsonToolbox](#module_MusicJsonToolbox)</code>  
+**Returns**: <code>Array</code> - An array containing all notes of the given object  
 
 | Param | Type | Description |
 | --- | --- | --- |
@@ -21,13 +27,81 @@ Returns an array of all notes (transposed to C major).Example:[ {object}, {ob
 
 <a name="module_MusicJsonToolbox.ngrams"></a>
 
-### MusicJsonToolbox.ngrams(array, length)
+### MusicJsonToolbox.ngrams(array, length) ⇒ <code>Array</code>
 Generates array of ngrams in specified length (based on [https://gist.github.com/eranbetzalel/9f16b1216931e20775ad](https://gist.github.com/eranbetzalel/9f16b1216931e20775ad)).
 
 **Kind**: static method of <code>[MusicJsonToolbox](#module_MusicJsonToolbox)</code>  
+**Returns**: <code>Array</code> - An Array of ngrams with the given length  
 
 | Param | Type | Description |
 | --- | --- | --- |
 | array | <code>Array</code> | An array of notes (returned by function "notes") |
-| length | <code>Number</code> | The length of n |
+| length | <code>number</code> | The length of n |
+
+<a name="module_MusicJsonToolbox.base12Pitch"></a>
+
+### MusicJsonToolbox.base12Pitch(step, octave, alter, absolute) ⇒ <code>number</code>
+Calculates the base 12 represented pitch
+
+**Kind**: static method of <code>[MusicJsonToolbox](#module_MusicJsonToolbox)</code>  
+**Returns**: <code>number</code> - The base12 pitch number  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| step | <code>string</code> | The step (c, d, e, f, g, a, b) |
+| octave | <code>number</code> | The octave |
+| alter | <code>number</code> | The value for alter (either from accidental or key) |
+| absolute | <code>bool</code> | When set, the octave is taken into account, otherwise function return relative value (from 1 to 12) |
+
+<a name="module_MusicJsonToolbox.comparePitch"></a>
+
+### MusicJsonToolbox.comparePitch(pitch1, pitch2, absolute) ⇒ <code>number</code>
+Calculates difference between two pitches
+
+**Kind**: static method of <code>[MusicJsonToolbox](#module_MusicJsonToolbox)</code>  
+**Returns**: <code>number</code> - The difference between two pitches  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| pitch1 | <code>object</code> | The first pitch to compare |
+| pitch2 | <code>object</code> | The second pitch to compare |
+| absolute | <code>bool</code> | When set, the octave is taken into account, otherwise function return relative value (from 1 to 12) |
+
+<a name="module_MusicJsonToolbox.compareDuration"></a>
+
+### MusicJsonToolbox.compareDuration(duration1, duration2) ⇒ <code>number</code>
+Calculates difference between two durations
+
+**Kind**: static method of <code>[MusicJsonToolbox](#module_MusicJsonToolbox)</code>  
+**Returns**: <code>number</code> - The difference between two durations  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| duration1 | <code>number</code> | The first duration to compare |
+| duration2 | <code>number</code> | The second duration to compare |
+
+<a name="module_MusicJsonToolbox.uniques"></a>
+
+### MusicJsonToolbox.uniques(array) ⇒ <code>Array</code>
+Returns only unique array values
+
+**Kind**: static method of <code>[MusicJsonToolbox](#module_MusicJsonToolbox)</code>  
+**Returns**: <code>Array</code> - Array with only unique values  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| array | <code>Array</code> | The array with possible duplicate values |
+
+<a name="module_MusicJsonToolbox.distanceNgrams"></a>
+
+### MusicJsonToolbox.distanceNgrams(object, search) ⇒ <code>object</code>
+Returns the minimum pseudo-edit-distance between the searched notes and corresponding ngrams
+
+**Kind**: static method of <code>[MusicJsonToolbox](#module_MusicJsonToolbox)</code>  
+**Returns**: <code>object</code> - The first finding with minimum cost  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| object | <code>object</code> | A musicjson object to search in |
+| search | <code>Array</code> | An array of notes that should be searched |
 
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 (function() {
   'use strict';
 
+  var base12 = {
+    "C": 1,
+    "D": 3,
+    "E": 5,
+    "F": 6,
+    "G": 8,
+    "A": 10,
+    "B": 12
+  };
+
   /**
    * The MusicJsonToolbox class implements static functions to operate with musicjson objects.
    * @exports MusicJsonToolbox
@@ -11,18 +21,26 @@
      *
      * Example:
      * [ {object}, {object}, ... ]
-     * 
+     *
      * @param {object} obj - The musicjson object
+     * @returns {Array} An array containing all notes of the given object
      */
     notes: function(obj) {
       var tempNotes = [];
       var repeatStart = -1;
 
       for (var i = 0; i < obj.measures.length; i++) {
+
+        for (var j = 0; j < obj.measures[i].notes.length; j++) {
+          obj.measures[i].notes[j].measure = i;
+        }
+
         if (obj.measures[i].attributes.repeat.left) {
           repeatStart = i;
         }
+
         tempNotes = tempNotes.concat(obj.measures[i].notes);
+
         if (obj.measures[i].attributes.repeat.right) {
           if (repeatStart !== -1) {
             while (repeatStart <= i) {
@@ -40,7 +58,8 @@
     /**
      * Generates array of ngrams in specified length (based on {@link https://gist.github.com/eranbetzalel/9f16b1216931e20775ad}).
      * @param {Array} array - An array of notes (returned by function "notes")
-     * @param {Number} length - The length of n
+     * @param {number} length - The length of n
+     * @returns {Array} An Array of ngrams with the given length
      */
     ngrams: function(array, length) {
       var nGramsArray = [];
@@ -56,6 +75,101 @@
       }
 
       return nGramsArray;
+    },
+
+    /**
+     * Calculates the base 12 represented pitch
+     * @param {string} step - The step (c, d, e, f, g, a, b)
+     * @param {number} octave - The octave
+     * @param {number} alter - The value for alter (either from accidental or key)
+     * @param {bool} absolute - When set, the octave is taken into account, otherwise function return relative value (from 1 to 12)
+     * @returns {number} The base12 pitch number
+     */
+    base12Pitch: function(step, octave, alter, absolute) {
+      if (absolute) {
+        return (base12[step] + octave * 12) + alter;
+      } else {
+        return base12[step] + alter;
+      }
+
+    },
+
+    /**
+     * Calculates difference between two pitches
+     * @param {object} pitch1 - The first pitch to compare
+     * @param {object} pitch2 - The second pitch to compare
+     * @param {bool} absolute - When set, the octave is taken into account, otherwise function return relative value (from 1 to 12)
+     * @returns {number} The difference between two pitches
+     */
+    comparePitch: function(pitch1, pitch2, absolute) {
+      if (typeof absolute === 'undefined') absolute = false;
+      return Math.abs(this.base12Pitch(pitch1.step, pitch1.octave, pitch1.alter, absolute) - this.base12Pitch(pitch2.step, pitch2.octave, pitch2.alter, absolute));
+    },
+
+    /**
+     * Calculates difference between two durations
+     * @param {number} duration1 - The first duration to compare
+     * @param {number} duration2 - The second duration to compare
+     * @returns {number} The difference between two durations
+     */
+    compareDuration: function(duration1, duration2) {
+      return Math.abs(duration1 - duration2);
+    },
+
+    /**
+     * Returns only unique array values
+     * @param {Array} array - The array with possible duplicate values
+     * @returns {Array} Array with only unique values
+     */
+    uniques: function(array) {
+      var a = [];
+      for (var i=0, l=array.length; i<l; i++)
+        if (a.indexOf(array[i]) === -1 && array[i] !== '')
+          a.push(array[i]);
+      return a;
+    },
+
+    /**
+     * Returns the minimum pseudo-edit-distance between the searched notes and corresponding ngrams
+     * @param {object} object - A musicjson object to search in
+     * @param {Array} search - An array of notes that should be searched
+     * @returns {object} The first finding with minimum cost
+     */
+    distanceNgrams: function(object, search) {
+      // TODO: improve from pseudo-edit-distance to real edit-distance
+      //       maybe comparing strings of base12 pitches
+      //       eg: 5 4 12 9 11
+      //           | |  | |  |
+      //           0 0  2 1  0
+      //           | |  | |  |
+      //           5 4 10 8 11
+      var ngrams = this.ngrams(this.notes(object), search.length);
+      var costs = [];
+      for (var i = 0; i < ngrams.length; i++) {
+        var tempCost = 0;
+        var tempMeasures = [];
+        for (var j = 0; j < search.length; j++) {
+          if (ngrams[i][j].rest || search[j].rest) {
+            tempCost = tempCost + this.compareDuration(ngrams[i][j].duration, search[j].duration);
+          } else {
+            tempCost = tempCost + this.comparePitch(ngrams[i][j].pitch, search[j].pitch) + this.compareDuration(ngrams[i][j].duration, search[j].duration);
+          }
+          if (!tempMeasures.hasOwnProperty(ngrams[i][j].measure)) {
+            tempMeasures.push(ngrams[i][j].measure);
+          }
+        }
+        costs.push({
+          cost: tempCost,
+          measures: this.uniques(tempMeasures).sort()
+        });
+      }
+
+      console.log(costs);
+
+      // TODO: return multiple values if same lowest cost!
+      return costs.sort(function (a, b) {
+        return b.cost - a.cost;
+      }).pop();
     }
   };
 
